175ba5db8508606319cc30c3c4553c411201c60b
Song Hyun Kim(Maria) <globalkmaria@gmail.com>
2023-04-03 01:17:12 +0900

    Add : Button Component - height

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -1,7 +1,8 @@
+import { ButtonHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
-const SIZE = ['s', 'm', 'l'] as const;
-type Sizes = { [key in typeof SIZE[number]]: string };
+const SIZES = ['s', 'm', 'l'] as const;
+type Sizes = { [key in typeof SIZES[number]]: string };
 const FONT_SIZES: Sizes = {
   s: '1rem',
   m: '1.25rem',
@@ -21,40 +22,43 @@ const COLORS: Colors = {
   warning: 'red',
 };
 
-interface BaseButtonProps {
-  size?: typeof SIZE[number];
+const HEIGHTS = [32, 40] as const;
+
+interface BaseButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: typeof SIZES[number];
   color?: typeof COLOR_KEYS[number];
+  height?: typeof HEIGHTS[number];
+  width?: number;
+  fullWidth?: boolean;
 }
 
-const BaseButton = styled('button')<BaseButtonProps>(({ size = 's' }) => {
-  return {
+const BaseButton = styled('button')<BaseButtonProps>(
+  ({ height = 40, width, fullWidth, size = 's' }) => ({
     padding: PADDING_SIZES[size],
+    height: `${height}px`,
+    width: width ? `${width}px` : fullWidth ? '100%' : 'auto',
     borderRadius: '5px',
     fontSize: FONT_SIZES[size],
     transition: '200ms',
-  };
-});
+  }),
+);
 
 export const BorderButton = styled(BaseButton)<BaseButtonProps>(
-  ({ theme, color = 'primary' }) => {
-    return {
-      border: `1px solid ${theme.colors[COLORS[color] + '200']}`,
+  ({ theme, color = 'primary' }) => ({
+    border: `1px solid ${theme.colors[COLORS[color] + '200']}`,
 
-      '&:hover': {
-        background: `${theme.colors[COLORS[color] + '100']}`,
-      },
-    };
-  },
+    '&:hover': {
+      background: `${theme.colors[COLORS[color] + '100']}`,
+    },
+  }),
 );
 
 export const ContainedButton = styled(BaseButton)<BaseButtonProps>(
-  ({ theme, color = 'primary' }) => {
-    return {
-      background: `${theme.colors[COLORS[color] + '200']}`,
+  ({ theme, color = 'primary' }) => ({
+    background: `${theme.colors[COLORS[color] + '200']}`,
 
-      '&:hover': {
-        background: `${theme.colors[COLORS[color] + '300']}`,
-      },
-    };
-  },
+    '&:hover': {
+      background: `${theme.colors[COLORS[color] + '300']}`,
+    },
+  }),
 );
