9bd7edc87950628b99671d928c77d8e525e63b62
Song Hyun Kim(Maria) <globalkmaria@gmail.com>
2023-04-04 19:56:00 +0900

    Add : Table Component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,10 +1,36 @@
 import React, { TableHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
+interface TableProps extends TableHTMLAttributes<HTMLTableElement> {}
+interface TableHeaderProps
+  extends TableHTMLAttributes<HTMLTableSectionElement> {}
+interface TableBodyProps extends TableHTMLAttributes<HTMLTableSectionElement> {}
 interface TableHeadProps extends TableHTMLAttributes<HTMLTableCellElement> {
   width?: number;
   flexBasis?: number;
 }
+interface TableRowProps extends TableHTMLAttributes<HTMLTableRowElement> {}
+interface TableCellProps extends TableHTMLAttributes<HTMLTableCellElement> {
+  width?: number;
+  flexBasis?: number;
+  align?: 'left' | 'center' | 'right';
+}
+
+export const Table = styled('table')<TableProps>(({ theme }) => ({
+  borderCollapse: 'collapse',
+  width: '100%',
+}));
+
+export const TableHeader = styled('thead')<TableHeaderProps>(({ theme }) => ({
+  height: '40px',
+  borderTop: `1px solid ${theme.colors.grey400}`,
+  borderBottom: `1px solid ${theme.colors.grey400}`,
+}));
+
+export const TableBody = styled('tbody')<TableBodyProps>(({ theme }) => ({
+  fontSize: '1rem',
+}));
+
 export const TableHead = styled('th')<TableHeadProps>(
   ({ theme, width, flexBasis }) => ({
     padding: '0.4rem',
@@ -22,22 +48,17 @@ export const TableHead = styled('th')<TableHeadProps>(
     },
 
     '&:nth-child(odd)': {
-      background: theme.colors.grey100,
+      background: theme.colors.grey300,
     },
   }),
 );
 
-interface TableRowProps extends TableHTMLAttributes<HTMLTableRowElement> {}
 export const TableRow = styled('tr')<TableRowProps>(({ theme }) => ({
   borderBottom: `1px solid ${theme.colors.grey400}`,
 }));
 
-interface TableCellProps extends TableHTMLAttributes<HTMLTableCellElement> {
-  width?: number;
-  flexBasis?: number;
-}
 export const TableCell = styled('td')<TableCellProps>(
-  ({ theme, width, flexBasis }) => ({
+  ({ theme, width, flexBasis, align = 'left' }) => ({
     padding: '0.4rem',
     ...(width ? { minWidth: width + 'px' } : { flex: 1 }),
     ...(flexBasis
@@ -47,25 +68,10 @@ export const TableCell = styled('td')<TableCellProps>(
           flexShrink: 0,
         }
       : {}),
+    textAlign: align,
 
     '&:nth-child(n+2)': {
       borderLeft: `1px solid ${theme.colors.grey400}`,
     },
   }),
 );
-
-interface TableHeadsProps
-  extends TableHTMLAttributes<HTMLTableSectionElement> {}
-export const TableHeads = styled('thead')<TableHeadsProps>(({ theme }) => ({
-  borderTop: `1px solid ${theme.colors.grey400}`,
-  borderBottom: `1px solid ${theme.colors.grey400}`,
-}));
-
-interface TableBodyProps extends TableHTMLAttributes<HTMLTableSectionElement> {}
-export const TableBody = styled('tbody')<TableBodyProps>(({ theme }) => ({}));
-
-interface TableProps extends TableHTMLAttributes<HTMLTableElement> {}
-export const Table = styled('table')<TableProps>(({ theme }) => ({
-  borderCollapse: 'collapse',
-  width: '100%',
-}));
