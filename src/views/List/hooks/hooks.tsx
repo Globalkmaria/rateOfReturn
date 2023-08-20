8544b0ea9325026696d8317b4d67b3078b02b30d
Maria <globalkmaria@gmail.com>
2023-08-21 07:01:36 +0900

    Change : useInitWithLocalData to return fuc

--- src/views/List/hooks/hooks.tsx
+++ src/views/List/hooks/hooks.tsx
@@ -6,12 +6,13 @@ import { getInitialCheckedItemsInfo } from '../../../features/checkedItems/utils
 import { initCheckedItems } from '../../../features/checkedItems/checkedItemsSlice';
 import { selectStockList } from '../../../features/stockList/selectors';
 import { selectGroups } from '../../../features/groups/selectors';
+import { selectIsLoggedIn } from '../../../features/user/selectors';
 
-export const useInitData = (setFirstLoad: (value: boolean) => void) => {
+export const useInitWithLocalData = () => {
   const dispatch = useDispatch();
   const stockList = useSelector(selectStockList);
 
-  useEffect(() => {
+  const initData = () => {
     const localStock = localStorage.getItem('stockList');
     const localGroups = localStorage.getItem('groups');
     localStock && dispatch(initStockList(JSON.parse(localStock)));
@@ -25,23 +26,27 @@ export const useInitData = (setFirstLoad: (value: boolean) => void) => {
       value: true,
     });
     dispatch(initCheckedItems(checkedItemsInfo));
-    setFirstLoad(false);
-  }, []);
+  };
+  return initData;
 };
 
 export const useSaveChangedGroupsData = (firstLoad: boolean) => {
   const groups = useSelector(selectGroups);
+  const isLoggedIn = useSelector(selectIsLoggedIn());
 
   useEffect(() => {
+    if (isLoggedIn) return;
     if (!firstLoad) localStorage.setItem('groups', JSON.stringify(groups));
-  }, [groups.groups]);
+  }, [groups.groups, isLoggedIn]);
 };
 
 export const useSaveChangedStocksData = (firstLoad: boolean) => {
   const stockList = useSelector(selectStockList);
+  const isLoggedIn = useSelector(selectIsLoggedIn());
 
   useEffect(() => {
+    if (isLoggedIn) return;
     if (!firstLoad)
       localStorage.setItem('stockList', JSON.stringify(stockList));
-  }, [stockList]);
+  }, [stockList, isLoggedIn]);
 };
