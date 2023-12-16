8f3d9806d8399920f4f1118b43d763073e2090a7
Song Hyun Kim(Maria) <75622611+Globalkmaria@users.noreply.github.com>
2023-12-17 02:34:13 +0900

    Release : 2.1.5 - Add tests, refactor, and fix purchased item input bug  (#26)
    * Refactor : Sign up validator
    
    * Refactor : remove console.log
    
    * Refactor : StockItem utils and add tests
    
    * Refactor : groups state filter
    
    * Remove : classname
    
    * Refactor : Remove classname from GroupSummary
    
    * Bugfix : show PurchasedInput buy date fully
    
    * Refactor : GroupSummary remove unused type
    
    * Refactor : List Header - remove classname split Components
    
    * Refactor : Change from 'styled-components'; to from 'styled-components/macro';
    
    * Refactor : getInitialCheckedItemsInfo
    
    * Refactor : getUserData helper function
    
    * Refactor : SummaryInfo - remove className
    
    * Fix : define className in CheckboxCell to wrap with styled Component
    
    * Fix : SummaryContent - add name to currentPrice input to fix change not working
    
    * Add : stock length validity
    
    * Add : Sign up - validity max schema
    
    * Bugfix : Stock List - when number is long -> nowarp , show all number
    
    * Add : 2.1.6 Readme
    
    * Bugfix : List - show all Buy Quantity

--- src/views/List/Header/const.ts
+++ src/views/List/Header/const.ts
@@ -12,7 +12,7 @@ export const HEADER_LIST: HeaderItemProps[] = [
   { id: '2', label: 'Stock Name', fixedWidth: 120, Component: StyledStockName, notTableHead: false },
   { id: '3', label: '#', fixedWidth: 50, Component: StyledBuyId, notTableHead: false },
   { id: '4', label: 'Buy Date', fixedWidth: 230 },
-  { id: '5', label: 'Buy Quantity', fixedWidth: 100 },
+  { id: '5', label: 'Buy Quantity', minWidth: 120 },
   { id: '6', label: 'Avg Buy Unit Price', minWidth: 120 },
   {
     id: '7',
