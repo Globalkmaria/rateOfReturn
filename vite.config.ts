0d380f59b11e56143cbc454dab6587493cdcd49b
Maria <globalkmaria@gmail.com>
2024-04-13 09:16:55 +0900

    ref : config splitFiles

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
 import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { splitFiles } from './configUtils';
 
 export default defineConfig({
   server: {
@@ -32,23 +33,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id: string) {
-          if (id.includes('react-router-dom') || id.includes('@remix-run') || id.includes('react-router')) {
-            return '@react-router';
-          } else if (id.includes('react-dom')) {
-            return 'react-dom';
-          } else if (id.includes('zod')) {
-            return 'zod';
-          } else if (id.includes('chart.js')) {
-            return 'chart';
-          } else if (id.includes('axios')) {
-            return 'axios';
-          } else if (id.includes('redux')) {
-            return 'redux';
-          } else if (id.includes('styled-components')) {
-            return 'styled-components';
-          } else if (id.includes('node_modules')) {
-            return 'node_modules';
-          }
+          return splitFiles(id);
         },
       },
     },
