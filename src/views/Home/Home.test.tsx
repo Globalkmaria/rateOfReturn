b7d994c81ea673de8c5329cae254e86a00bf7579
Maria <globalkmaria@gmail.com>
2024-04-26 01:52:50 +0900

    feat : add Home page, Group Summary test

--- src/views/Home/Home.test.tsx
+++ src/views/Home/Home.test.tsx
@@ -16,31 +16,40 @@ createServer([
   },
 ]);
 
-describe('Home Component', () => {
-  test('Home render and Show Card', async () => {
+describe('Home page', () => {
+  test('Cards are showed correctly', async () => {
     renderWithProviders(<Home />);
     expect(await screen.findByRole('heading', { name: /apple/i })).toBeInTheDocument();
   });
 
   test('Expand Card when Card is clicked and close when clicked again', async () => {
+    const user = userEvent.setup();
     renderWithProviders(<Home />);
+
     const description = TOP_STOCKS[0].description;
     expect(screen.queryByText(description)).not.toBeInTheDocument();
 
-    await userEvent.click(await screen.findByRole('heading', { name: /apple/i }));
+    let apple = await screen.findByRole('heading', { name: /apple/i });
+
+    await user.click(apple);
     expect(await screen.findAllByLabelText(/expand close button/i)).toHaveLength(1);
     expect(await screen.findByText(description)).toBeInTheDocument();
 
-    await userEvent.click(await screen.findByRole('heading', { name: /apple/i }));
+    apple = await screen.findByRole('heading', { name: /apple/i });
+    await user.click(apple);
     expect(await screen.findAllByLabelText(/expand open button/i)).toHaveLength(3);
   });
 
   test(`When invest link is clicked Card doesn't change it's expand state`, async () => {
+    const user = userEvent.setup();
     renderWithProviders(<Home />);
 
-    expect(await screen.findAllByLabelText(/expand open button/i)).toHaveLength(3);
+    let expendButton = await screen.findAllByLabelText(/expand open button/i);
+    expect(expendButton).toHaveLength(3);
 
-    await userEvent.click((await screen.findAllByRole('link', { name: 'investing.com' }))[0]);
-    expect(await screen.findAllByLabelText(/expand open button/i)).toHaveLength(3);
+    const link = (await screen.findAllByRole('link', { name: 'investing.com' }))[0];
+    await user.click(link);
+    expendButton = await screen.findAllByLabelText(/expand open button/i);
+    expect(expendButton).toHaveLength(3);
   });
 });
