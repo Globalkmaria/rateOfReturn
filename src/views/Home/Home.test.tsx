dd91ef947863d923790d5c0511e7040dfa49d313
Maria <globalkmaria@gmail.com>
2024-04-26 15:54:39 +0900

    fix : Home test to use length

--- src/views/Home/Home.test.tsx
+++ src/views/Home/Home.test.tsx
@@ -29,13 +29,12 @@ describe('Home page', () => {
     const description = TOP_STOCKS[0].description;
     expect(screen.queryByText(description)).not.toBeInTheDocument();
 
-    let apple = await screen.findByRole('heading', { name: /apple/i });
+    const apple = await screen.findByRole('heading', { name: /apple/i });
 
     await user.click(apple);
     expect(await screen.findAllByLabelText(/expand close button/i)).toHaveLength(1);
     expect(await screen.findByText(description)).toBeInTheDocument();
 
-    apple = await screen.findByRole('heading', { name: /apple/i });
     await user.click(apple);
     expect(await screen.findAllByLabelText(/expand open button/i)).toHaveLength(3);
   });
@@ -44,12 +43,12 @@ describe('Home page', () => {
     const user = userEvent.setup();
     renderWithProviders(<Home />);
 
-    let expendButton = await screen.findAllByLabelText(/expand open button/i);
-    expect(expendButton).toHaveLength(3);
+    const expendButton = await screen.findAllByLabelText(/expand open button/i);
+    const expendedLength = expendButton.length;
 
     const link = (await screen.findAllByRole('link', { name: 'investing.com' }))[0];
     await user.click(link);
-    expendButton = await screen.findAllByLabelText(/expand open button/i);
-    expect(expendButton).toHaveLength(3);
+    const afterExpendButton = await screen.findAllByLabelText(/expand open button/i);
+    expect(afterExpendButton).toHaveLength(expendedLength);
   });
 });
