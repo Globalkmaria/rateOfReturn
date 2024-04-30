75aea9bb3cf4764af59de236f54eafd925f0a72a
Maria <globalkmaria@gmail.com>
2024-05-01 05:48:45 +0900

    fix : Dropbox - width max-content

--- src/components/Dropbox/Dropbox.tsx
+++ src/components/Dropbox/Dropbox.tsx
@@ -33,7 +33,8 @@ const Container = styled.div.withConfig({
   }),
 
   padding: '5px 0',
-  width: width ? `${width}px` : 'auto',
+  width: 'max-content',
+  minWidth: width ? `${width}px` : '',
   backgroundColor: theme.colors.white,
   boxShadow: `rgba(50, 50, 93, 0.25) 1px -1px 5px -1px, rgba(0, 0, 0, 0.3) -1px 1px 3px -1px`,
   borderRadius: '5px',
