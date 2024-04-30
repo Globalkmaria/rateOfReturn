24bbf88ea480dc6368720e933cc4254a81a46170
Maria <globalkmaria@gmail.com>
2024-05-01 00:12:47 +0900

    fix : DropdownItem disable css

--- src/components/Dropdown/DropdownItem.tsx
+++ src/components/Dropdown/DropdownItem.tsx
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const DropdownItem = styled.button.attrs({ type: 'button' })``;
+export const DropdownItem = styled.button.attrs({ type: 'button' })`
+  &&:disabled {
+    cursor: default;
+    color: ${({ theme }) => theme.colors.grey400};
+    background: transparent;
+  }
+`;
