915499ecee2a45e84a4992ed857996748d71c964
Maria <globalkmaria@gmail.com>
2024-05-05 06:15:20 +0900

    fix : now reset remote data available

--- src/views/List/Backup/ResetDataWarning.tsx
+++ src/views/List/Backup/ResetDataWarning.tsx
@@ -1,7 +1,12 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { resetUserData } from '@/features';
 import WarningModal from '@/components/WarningModal';
+import { selectIsLoggedIn } from '@/features/user/selectors';
+import userDataService from '@/service/userData/userData';
+import { STOCK_INITIAL_STATE } from '@/features/stockList/stockListSlice';
+import { GROUP_INITIAL_STATE } from '@/features/groups/groupsSlice';
+import { SOLD_INITIAL_STATE } from '@/features/solds';
 
 type Props = {
   onClose: () => void;
@@ -9,20 +14,54 @@ type Props = {
 
 const ResetDataWarning = ({ onClose }: Props) => {
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  const onReset = () => {
+  const onReset = async () => {
+    if (isLoggedIn) {
+      const result = await userDataService.replaceUserData({
+        stocks: {
+          stocks: {},
+          nextStockId: STOCK_INITIAL_STATE.nextStockId,
+          nextItemId: STOCK_INITIAL_STATE.nextPurchasedId,
+        },
+        groups: {
+          groups: {},
+          nextGroupId: GROUP_INITIAL_STATE.nextGroupId,
+        },
+        solds: {
+          solds: {},
+          nextId: SOLD_INITIAL_STATE.nextId,
+        },
+      });
+
+      if (!result.success) {
+        alert('Failed to reset remote data.');
+        return;
+      }
+    }
+
     dispatch(resetUserData());
     onClose();
   };
 
-  return <WarningModal onClose={onClose} onConfirm={onReset} message={MESSAGE} buttonName='Reset' />;
+  return (
+    <WarningModal
+      onClose={onClose}
+      onConfirm={onReset}
+      message={MESSAGE}
+      buttonName='Reset'
+    />
+  );
 };
 
 export default ResetDataWarning;
 
 const MESSAGE = (
   <>
-    If you reset, current data will be deleted.
+    If you reset, current data will be deleted. <br /> Please note that this
+    action is irreversible. <br />
+    If you want to get backup file before reset, click the 'Get Backup File'
+    first.
     <br /> Are you sure you want to reset?
   </>
 );
