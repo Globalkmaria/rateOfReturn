fc64dedeef179c0c46feacc7f4c7c7e9f7410870
Maria <globalkmaria@gmail.com>
2024-05-05 02:55:08 +0900

    ref : selector

--- src/features/checkedItems/selectors.ts
+++ src/features/checkedItems/selectors.ts
@@ -2,19 +2,15 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../../store';
 
 export const selectCheckedItems = (state: RootState) => state.checkedItems;
-export const selectCheckItemsInfo = createSelector(
-  [selectCheckedItems],
-  (checkedItemsInfo) => checkedItemsInfo.stocksCheckInfo,
-);
-export const selectIsAllChecked = createSelector(
-  [selectCheckedItems],
-  (checkedItems) => checkedItems.allChecked,
-);
+export const selectCheckItemsInfo = (state: RootState) =>
+  state.checkedItems.stocksCheckInfo;
+export const selectIsAllChecked = (state: RootState) =>
+  state.checkedItems.allChecked;
 
 export const selectStockCheckedInfo = (stockId: string) =>
   createSelector(
     [selectCheckItemsInfo],
-    (stocksCheckInfo) => stocksCheckInfo[stockId],
+    stocksCheckInfo => stocksCheckInfo[stockId],
   );
 export const selectIsPurchasedItemChecked = (
   stockId: string,
@@ -22,18 +18,18 @@ export const selectIsPurchasedItemChecked = (
 ) =>
   createSelector(
     [selectStockCheckedInfo(stockId)],
-    (checkedStockInfo) => checkedStockInfo.purchasedItems[purchasedId],
+    checkedStockInfo => checkedStockInfo.purchasedItems[purchasedId],
   );
 
 export const selectCheckedPurchasedItems = createSelector(
   [selectCheckItemsInfo],
-  (stocksCheckInfo) => {
+  stocksCheckInfo => {
     const checkedPurchasedItems: { stockId: string; purchasedId: string }[] =
       [];
 
-    Object.keys(stocksCheckInfo).forEach((stockId) => {
+    Object.keys(stocksCheckInfo).forEach(stockId => {
       const purchasedItems = stocksCheckInfo[stockId].purchasedItems;
-      Object.keys(purchasedItems).forEach((purchasedId) => {
+      Object.keys(purchasedItems).forEach(purchasedId => {
         if (purchasedItems[purchasedId]) {
           checkedPurchasedItems.push({ stockId, purchasedId });
         }
