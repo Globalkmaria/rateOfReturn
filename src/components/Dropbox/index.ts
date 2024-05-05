390319ff6ee4f353d4d83dc3c24d0213eaa3107a
Maria <globalkmaria@gmail.com>
2024-05-05 13:15:13 +0900

    feat : add DropboxWrapper

---
+++ src/components/Dropbox/index.ts
@@ -0,0 +1,11 @@
+import { default as DropboxContainer } from './Dropbox';
+import { DropboxWrapper } from './DropboxWrapper';
+import { DropboxItem } from './DropboxItem';
+
+const Dropbox = {
+  Wrapper: DropboxWrapper,
+  Container: DropboxContainer,
+  Item: DropboxItem,
+};
+
+export default Dropbox;
