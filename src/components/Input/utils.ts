0d14ed68011333ebb10425be8e9ac8a842621a30
Maria <globalkmaria@gmail.com>
2024-05-12 00:19:09 +0900

    fix : input Component - decimal logic

--- src/components/Input/utils.ts
+++ src/components/Input/utils.ts
@@ -1,71 +1,118 @@
-import { INPUT_TYPES, TransformedValue } from './BaseInput';
+import { ChangeEvent } from 'react';
+import { InputType, InputValidation, TransformedValue } from './BaseInput';
+import { removeComma } from '@/utils';
 
-export const changeTransformValue = (
-  type: (typeof INPUT_TYPES)[number],
-  value: string,
-  prevValue: string,
-): TransformedValue => {
-  switch (type) {
-    case 'number':
-      const checkMaxDotToBeOne = value
-        .replace(/\.{1,}/g, '.')
-        .replace(/[^\.]/g, '').length;
-      if (checkMaxDotToBeOne > 1) return null;
+const checkValidDots = (value: string) => {
+  if (value[0] === '.') return false;
 
-      const [integer, decimal] = value
-        .replace(/\.{1,}/g, '.')
-        .replace(/\s/g, '')
-        .split('.');
-
-      const num = Number(integer.replaceAll(',', ''));
-
-      const isValid = !isNaN(num) && !isNaN(Number(decimal || 0));
-      if (!isValid) return null;
-      const noDot = decimal === undefined;
-
-      const localValue = num.toLocaleString() + (noDot ? '' : `.${decimal}`);
-      const pureValue = num.toString() + (noDot ? '' : `.${decimal}`);
-      return prevValue !== localValue ? [localValue, pureValue] : null;
-    default:
-      return prevValue !== value ? [value, value] : null;
-  }
+  const dots = value.split('.').length - 1;
+  return dots <= 1;
 };
 
-export const blurTransformValue = (
-  type: (typeof INPUT_TYPES)[number],
-  value: string,
-  prevValue: string,
-): TransformedValue => {
-  switch (type) {
-    case 'number':
-      const [integer, decimal] = value.split('.');
-      const num = Number(integer.replaceAll(',', ''));
-      const finalDecimal =
-        decimal === undefined
-          ? ''
-          : decimal.length < 2
-          ? `.${decimal.padEnd(2, '0')}`
-          : `.${decimal}`;
-      const localValue = num.toLocaleString() + finalDecimal;
-      const pureValue = num.toString() + finalDecimal;
-      return prevValue !== localValue ? [localValue, pureValue] : null;
-    default:
-      return prevValue !== value ? [value, value] : null;
+const getLocalString = (commaRemovedValue: string) => {
+  if (commaRemovedValue.includes('.')) {
+    const [integer, decimal = ''] = commaRemovedValue.split('.');
+    return `${Number(integer).toLocaleString()}.${decimal}`;
   }
+
+  return Number(commaRemovedValue).toLocaleString();
 };
 
-export const getInitialValue = (
+const removeInvalidedEdgeChars = (value: string) => {
+  value = value.trim();
+
+  let validStart = 0;
+  for (let i = 0; i < value.length; i++) {
+    if (!isNaN(Number(value[i]))) {
+      validStart = i;
+      break;
+    }
+  }
+
+  let validEnd = value.length - 1;
+  for (let i = value.length - 1; i >= 0; i--) {
+    if (!isNaN(Number(value[i]))) {
+      validEnd = i;
+      break;
+    }
+  }
+
+  return value.slice(validStart, validEnd + 1);
+};
+
+const trimInsertPaste = (
   value: string,
-  type: (typeof INPUT_TYPES)[number],
+  validation: (value: any) => boolean,
+): TransformedValue => {
+  const cleanedValue = removeInvalidedEdgeChars(value);
+  const commaRemovedValue = removeComma(cleanedValue);
+
+  const numValue = Number(commaRemovedValue);
+  if (isNaN(numValue)) return null;
+
+  const isValid = validation(numValue);
+  if (!isValid) return null;
+
+  const localValue = getLocalString(commaRemovedValue);
+
+  return [localValue, numValue];
+};
+
+const trimInsertText = (
+  value: string,
+  validation: InputValidation,
+): TransformedValue => {
+  const validDots = checkValidDots(value);
+  if (!validDots) return null;
+
+  const commaRemovedValue = removeComma(value);
+
+  const numValue = Number(commaRemovedValue);
+  if (isNaN(numValue)) return null;
+
+  const isValid = validation(numValue);
+  if (!isValid) return null;
+
+  const localValue = getLocalString(commaRemovedValue);
+
+  return [localValue, numValue];
+};
+
+const handleDecimalChange = (
+  e: ChangeEvent<HTMLInputElement>,
+  validation: InputValidation,
+): TransformedValue => {
+  const value = e.target.value;
+  if (value === '') return ['', 0];
+
+  const inputType = (e.nativeEvent as InputEvent).inputType;
+
+  if (inputType === 'insertFromPaste') {
+    return trimInsertPaste(value, validation);
+  }
+
+  return trimInsertText(value, validation);
+};
+
+const handleChange = (
+  e: ChangeEvent<HTMLInputElement>,
+  validation: InputValidation,
 ) => {
+  const isValid = validation(e.target.value);
+  if (!isValid) return null;
+  return e.target.value;
+};
+
+export const getTransformedValue = (
+  e: ChangeEvent<HTMLInputElement>,
+  validation: InputValidation,
+  type: InputType,
+): TransformedValue => {
   switch (type) {
-    case 'number':
-      const [integer, decimal] = value.split('.');
-      return (
-        Number(integer.replaceAll(',', '')).toLocaleString() +
-        (decimal !== undefined ? `.${decimal}` : '')
-      );
+    case 'decimal':
+      return handleDecimalChange(e, validation);
+
     default:
-      return value;
+      return handleChange(e, validation);
   }
 };
