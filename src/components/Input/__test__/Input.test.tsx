aeb664582f29bd49416d0f0a686b66d76b065fe7
Maria <globalkmaria@gmail.com>
2024-05-15 23:21:56 +0900

    fix : Input test

--- src/components/Input/__test__/Input.test.tsx
+++ src/components/Input/__test__/Input.test.tsx
@@ -1,23 +1,88 @@
 import { screen } from '@testing-library/react';
 
-import { Input } from '../Input';
 import { renderWithStyle } from '../../../__test__/renderUI';
+import userEvent from '@testing-library/user-event';
+import TestInput from './TestInput';
 
 describe('Input component', () => {
-  let onChange: jest.Mock;
-  let onBlur: jest.Mock;
+  let user: ReturnType<typeof userEvent.setup>;
 
-  beforeEach(() => {
-    onChange = jest.fn();
-    onBlur = jest.fn();
+  beforeAll(() => {
+    user = userEvent.setup();
+  });
+
+  describe('when type is decimal', () => {
+    test('have attribute text and initial value to be empty', () => {
+      renderWithStyle(<TestInput type='decimal' />);
+      const input = screen.getByRole('textbox');
+      expect(input).toHaveAttribute('type', 'text');
+      expect(input).toHaveAttribute('value', '');
+    });
+
+    test('Add a comma after every 3 digits as a separator', async () => {
+      renderWithStyle(<TestInput type='decimal' />);
+
+      const input = screen.getByRole('textbox');
+      await user.click(input);
+      await user.type(input, '1234');
+      expect(input).toHaveAttribute('value', '1,234');
+    });
+
+    test('Can add dot', async () => {
+      renderWithStyle(<TestInput type='decimal' />);
+      const input = screen.getByRole('textbox');
+      await user.click(input);
+      await user.type(input, '1234.');
+      expect(input).toHaveAttribute('value', '1,234.');
+    });
+
+    test('Cannot add more than 1 dot', async () => {
+      renderWithStyle(<TestInput type='decimal' />);
+      const input = screen.getByRole('textbox');
+      await user.click(input);
+      await user.type(input, '1234..');
+      expect(input).toHaveAttribute('value', '1,234.');
+    });
+
+    test('ignore letters that are not dot and number', async () => {
+      renderWithStyle(<TestInput type='decimal' />);
+      const input = screen.getByRole('textbox');
+      await user.click(input);
+      await user.type(input, 'a');
+      expect(input).toHaveAttribute('value', '');
+    });
   });
 
   describe('when type is number', () => {
-    test('have attribute text and initial value to be 0', () => {
-      renderWithStyle(<Input type='number' onChange={onChange} onBlur={onBlur} />);
+    test('have attribute text and initial value to be empty', () => {
+      renderWithStyle(<TestInput type='number' />);
       const input = screen.getByRole('textbox');
       expect(input).toHaveAttribute('type', 'text');
-      expect(input).toHaveAttribute('value', '0');
+      expect(input).toHaveAttribute('value', '');
+    });
+
+    test('Add a comma after every 3 digits as a separator', async () => {
+      renderWithStyle(<TestInput type='number' />);
+      const input = screen.getByRole('textbox');
+      await user.click(input);
+      await user.type(input, '1234');
+      expect(input).toHaveAttribute('value', '1,234');
+    });
+
+    test('ignore letters that are not number', async () => {
+      renderWithStyle(<TestInput type='number' />);
+      const input = screen.getByRole('textbox');
+      await user.click(input);
+      await user.type(input, 'a');
+      expect(input).toHaveAttribute('value', '');
+    });
+
+    test('ignore dot', async () => {
+      renderWithStyle(<TestInput type='number' />);
+      const input = screen.getByRole('textbox');
+      await user.click(input);
+      await user.type(input, '.');
+      expect(input).toHaveAttribute('value', '');
     });
   });
 });
