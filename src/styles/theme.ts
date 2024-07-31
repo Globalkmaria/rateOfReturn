20a8bb4ceaf76575dcee91a243210bd789181d4d
Maria <globalkmaria@gmail.com>
2024-07-31 01:54:06 +0100

    feat : theme - device screen

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -102,6 +102,7 @@ const devices = {
   tablet: `(max-width: 768px)`,
   laptop: `(max-width: 992px)`,
   laptopL: `(max-width: 1128px)`,
+  screen: `(max-width: 1640px)`,
 };
 
 export type ColorsTypes = typeof colors;
