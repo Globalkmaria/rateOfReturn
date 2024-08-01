031799661fd22f4084c82639b63d990fab030507
Maria <globalkmaria@gmail.com>
2024-08-01 19:51:20 +0100

    fix : Chart/Portfolio - chang url when selected group is changed

--- src/utils/group.ts
+++ src/utils/group.ts
@@ -1,3 +1,6 @@
 import { MAIN_GROUP_ID } from '@/features/groups/mockData';
 
 export const checkIfMainGroup = (groupId: string) => groupId === MAIN_GROUP_ID;
+
+export const validateGroupId = (groupId: string, groupIds: string[]) =>
+  groupId === MAIN_GROUP_ID || groupIds.includes(groupId);
