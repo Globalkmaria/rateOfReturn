909f920148d5e0bea09854bb8366fe6aab829f09
Maria <globalkmaria@gmail.com>
2024-08-09 00:59:02 +0100

    fix : useModal - can add name

--- src/views/List/hooks/useModal.tsx
+++ src/views/List/hooks/useModal.tsx
@@ -1,21 +1,41 @@
 import { useCallback, useState } from 'react';
 
-function useModal() {
+type ModalHandlers<T extends string | undefined> = T extends string
+  ? {
+      [K in `showModal${Capitalize<T>}`]: boolean;
+    } & {
+      [K in `onOpenModal${Capitalize<T>}`]: () => void;
+    } & {
+      [K in `onCloseModal${Capitalize<T>}`]: () => void;
+    } & {
+      [K in `onToggleModal${Capitalize<T>}`]: () => void;
+    }
+  : {
+      showModal: boolean;
+      onOpenModal: () => void;
+      onCloseModal: () => void;
+      onToggleModal: () => void;
+    };
+
+function useModal<T extends string | undefined = undefined>(
+  name?: T,
+): ModalHandlers<T> {
   const [showModal, setShowModal] = useState(false);
 
   const onOpenModal = useCallback(() => setShowModal(true), []);
   const onCloseModal = useCallback(() => setShowModal(false), []);
-  const onToggleModal = useCallback(
-    () => setShowModal(!showModal),
-    [showModal],
-  );
+  const onToggleModal = useCallback(() => setShowModal(prev => !prev), []);
+
+  const capitalized = capitalize(name ?? '');
 
   return {
-    showModal,
-    onOpenModal,
-    onCloseModal,
-    onToggleModal,
-  };
+    [`showModal${capitalized}`]: showModal,
+    [`onOpenModal${capitalized}`]: onOpenModal,
+    [`onCloseModal${capitalized}`]: onCloseModal,
+    [`onToggleModal${capitalized}`]: onToggleModal,
+  } as ModalHandlers<T>;
 }
 
 export default useModal;
+
+const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
