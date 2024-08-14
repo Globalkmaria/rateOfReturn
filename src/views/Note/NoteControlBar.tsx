7470114b0e4e4f048822a05ad50e8dc5daf5c325
Maria <globalkmaria@gmail.com>
2024-08-15 00:54:32 +0100

    feat : Note/ControlBar - add filter and search UI

--- src/views/Note/NoteControlBar.tsx
+++ src/views/Note/NoteControlBar.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 import { BorderButton } from '@/components/Button';
 import Icon from '@/components/Icon';
+import Search from '@/components/Search';
+import RadioSelect from '@/components/RadioSelect';
+import { mockFn } from '@/utils/mock';
 
 import useModal from '../List/hooks/useModal';
 import AddNote from './NoteInfo/modals/AddNote';
@@ -17,11 +20,59 @@ function NoteControlBar({ disabled }: Props) {
   return (
     <>
       <StyledNoteControlBar>
-        <BorderButton size='s' onClick={onOpenModal} disabled={disabled}>
+        <StyledSearch
+          width='100%'
+          value={''}
+          placeholder={SEARCH_PLACEHOLDER_TEXT}
+          height='s'
+          onChange={() => {}}
+        />
+        <StyledFilters>
+          <RadioSelect
+            label='Stock name'
+            title='Filter by stock name'
+            value={''}
+            options={[]}
+            onClick={mockFn}
+          />
+          <RadioSelect
+            label='Stock Id'
+            title='Filter by stock id'
+            value={''}
+            options={[]}
+            onClick={mockFn}
+          />
+          <RadioSelect
+            label='Sold Id'
+            title='Filter by sold id'
+            value={''}
+            options={[]}
+            onClick={mockFn}
+          />
+          <RadioSelect
+            label='Tag'
+            title='Filter by tag'
+            value={''}
+            options={[]}
+            onClick={mockFn}
+          />
+          <RadioSelect
+            label='Sort'
+            title='Sort by'
+            value={''}
+            options={[
+              { value: 'createdAt', label: 'created at ascending' },
+              { value: 'updatedAt', label: 'Newest' },
+            ]}
+            onClick={mockFn}
+          />
+        </StyledFilters>
+        <StyledNewButton size='s' onClick={onOpenModal} disabled={disabled}>
           <Icon icon='add' disabled={disabled} />
-          <span>Add New</span>
-        </BorderButton>
+          <span>New Note</span>
+        </StyledNewButton>
       </StyledNoteControlBar>
+
       {showModal && (
         <AddNote
           initialFormState={INITIAL_NOTE_FORM_STATE}
@@ -34,21 +85,45 @@ function NoteControlBar({ disabled }: Props) {
 
 export default NoteControlBar;
 
+const SEARCH_PLACEHOLDER_TEXT = 'Search by title...';
+
 const StyledNoteControlBar = styled.div`
   padding: 20px 40px 0;
-  display: flex;
-  justify-content: flex-end;
+  display: grid;
   gap: 10px;
+  grid-template-columns: 1fr auto auto;
+  grid-template-areas: 'title filters new-button';
 
   & > ${BorderButton} {
     gap: 5px;
+  }
 
-    /* span {
-      color: inherit;
-    } */
+  @media ${({ theme }) => theme.devices.laptop} {
+    grid-template-areas: 'title new-button' 'filters filters';
+    grid-template-columns: 1fr auto;
+    grid-template-rows: auto auto;
   }
 
   @media ${({ theme }) => theme.devices.mobile} {
     padding: 20px 20px 0;
   }
+`;
+
+const StyledSearch = styled(Search)`
+  grid-area: title;
+`;
+
+const StyledFilters = styled.div`
+  grid-area: filters;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+
+  @media ${({ theme }) => theme.devices.laptop} {
+    justify-content: flex-end;
+  }
+`;
+
+const StyledNewButton = styled(BorderButton)`
+  grid-area: new-button;
 `;
