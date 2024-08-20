a017ff8e646aae5aabfc128fb11f3389015d60ec
Maria <globalkmaria@gmail.com>
2024-08-20 23:13:47 +0100

    fix : merge - modal only open once after login

--- src/views/Home/MergeLocalDataModal.tsx
+++ src/views/Home/MergeLocalDataModal.tsx
@@ -5,6 +5,8 @@ import { ContainedButton } from '../../components/Button';
 import userDataService from '../../service/userData/userData';
 import useGetUserData from '../List/hooks/useGetUserData';
 import { getLocalState } from './utils';
+import { useState } from 'react';
+import { setLocalStorageItem } from '@/utils/getLocalStorage';
 
 type Props = {
   onClose: () => void;
@@ -13,34 +15,58 @@ type Props = {
 const MergeLocalDataModal = ({ onClose }: Props) => {
   const { getUserData } = useGetUserData();
   const localState = getLocalState();
+  const [loading, setLoading] = useState(false);
+
+  const mergeButtonLabel = loading ? 'Merging...' : 'Merge Data';
 
   const onMerge = async () => {
+    if (loading) return;
+
+    setLoading(true);
     const result = await userDataService.mergeUserData(localState);
 
-    if (!result.success) return;
+    if (!result.success) {
+      setLoading(false);
+      return;
+    }
+
     getUserData();
     localStorage.clear();
+    setLocalStorageItem('merge', true);
 
+    setLoading(false);
     onClose();
   };
 
   const onDismiss = () => {
     localStorage.clear();
+    setLocalStorageItem('merge', true);
+
+    onClose();
+  };
+
+  const onModalClose = () => {
+    if (loading) return;
     onClose();
   };
 
   return (
-    <Modal title='Data Sync' onClose={onClose}>
+    <Modal title='Data Sync' onClose={onModalClose}>
       <div>
         <StyledMessage>
           You have local data that is not synced with the server. Would you like
           to merge it with the server data?
         </StyledMessage>
         <StyledButtonGroup>
-          <ContainedButton color='secondary1' size='m' onClick={onMerge}>
-            Merge data
+          <ContainedButton
+            disabled={loading}
+            color='secondary1'
+            size='m'
+            onClick={onMerge}
+          >
+            {mergeButtonLabel}
           </ContainedButton>
-          <ContainedButton size='m' onClick={onDismiss}>
+          <ContainedButton disabled={loading} size='m' onClick={onDismiss}>
             Dismiss
           </ContainedButton>
         </StyledButtonGroup>
