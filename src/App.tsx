275140f254c7e66751af6bdca9a02a9f7c63cad4
Song Hyun Kim <globalkmaria@gmail.com>
2025-02-20 22:26:49 +0900

    feat: add ErrorBoundary component for error handling in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 
 import GlobalStyles from './styles/GlobalStyles';
 import { theme } from './styles/theme';
+
 import { store } from './store';
-import { Provider } from 'react-redux';
 import { routers } from './router/router';
+import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
   return (
-    <>
+    <ErrorBoundary>
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <GlobalStyles />
@@ -17,7 +19,7 @@ function App() {
         </ThemeProvider>
       </Provider>
       <div id='portal-container' />
-    </>
+    </ErrorBoundary>
   );
 }
 
