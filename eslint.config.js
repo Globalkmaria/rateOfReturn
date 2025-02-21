178e2d4fed001146d7073c6559fab7ab881c4c15
Song Hyun Kim <globalkmaria@gmail.com>
2025-02-21 14:11:05 +0900

    feat: add ESLint configuration and scripts for linting and fixing code

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      'jsx-a11y': eslintPluginJsxA11y,
+      prettier: eslintPluginPrettier,
+      '@typescript-eslint': eslintPluginTs,
+    },
+    rules: {
+      ...eslintPluginReact.configs.recommended.rules,
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      ...eslintPluginJsxA11y.configs.recommended.rules,
+      ...eslintPluginTs.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'jsx-quotes': ['error', 'prefer-single'],
+      'arrow-parens': ['error', 'as-needed'],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+        runtime: 'automatic',
+      },
+    },
+  },
+];
