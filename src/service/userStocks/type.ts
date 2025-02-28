b9d1af63d617829f629b63b2ca42f42034cc9687
Song Hyun Kim <globalkmaria@gmail.com>
2025-03-01 01:34:55 +0900

    fix : remove unused types

--- src/service/userStocks/type.ts
+++ src/service/userStocks/type.ts
@@ -1,10 +1,5 @@
 import { PurchasedItemInfo } from '../../features/stockList/type';
-import { EditUserItemRepReq } from '../../repository/userStocks/type';
-//
-export type EditUserItemServiceData = Partial<
-  Omit<PurchasedItemInfo, 'purchasedId' | 'needInit'>
->;
 
-export type EditUserItemServiceReq = {
-  data: EditUserItemServiceData;
-} & Omit<EditUserItemRepReq, 'data'>;
+export type EditUserItemServiceData = Partial<
+  Omit<PurchasedItemInfo, 'purchasedId'>
+>;
