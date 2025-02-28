6bf01850d151a1111adbaeb5e40ecda4dd280f17
Song Hyun Kim <globalkmaria@gmail.com>
2025-03-01 02:03:41 +0900

    fix(stock list page): edit button -  prevent edit mode when stock list is empty

--- src/views/List/StockListContent/StockListEditButton.tsx
+++ src/views/List/StockListContent/StockListEditButton.tsx
@@ -1,7 +1,7 @@
 import { useTransition } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { selectStockList } from '@/features/stockList/selectors';
+import { selectStocks } from '@/features/stockList/selectors';
 import { updateStockList } from '@/features/stockList/stockListSlice';
 import { selectTemporalStockList } from '@/features/temporalStockList/selectors';
 import {
@@ -17,37 +17,42 @@ import { logInSaveStock } from './helper';
 function StockListEditButton() {
   const dispatch = useDispatch();
   const temporalStockList = useSelector(selectTemporalStockList);
-  const stockList = useSelector(selectStockList);
+  const stockList = useSelector(selectStocks);
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isLock = !temporalStockList.isEditMode;
   const [isLoading, startTransition] = useTransition();
 
-  const disable = isLoading || isLock;
+  const disabled = isLoading || isLock || !stockList.allIds.length;
 
   const toggleLock = async () => {
     if (isLock) {
       dispatch(updateTemporalStockListEditMode(true));
-    } else {
-      const keys = Object.keys(temporalStockList.stockList);
-      if (!keys.length) return;
-
-      startTransition(async () => {
-        if (isLoggedIn) {
-          await logInSaveStock({
-            changedStockData: temporalStockList.stockList,
-            originalStockData: stockList.stocks,
-          });
-        }
-
-        dispatch(updateStockList(temporalStockList.stockList));
-
-        dispatch(updateTemporalStockListEditMode(false));
-        dispatch(resetTemporalStockList());
-      });
+      return;
     }
+
+    if (!Object.keys(temporalStockList.stockList).length) {
+      dispatch(updateTemporalStockListEditMode(false));
+      return;
+    }
+
+    startTransition(async () => {
+      if (isLoggedIn) {
+        await logInSaveStock({
+          changedStockData: temporalStockList.stockList,
+          originalStockData: stockList,
+        });
+      }
+
+      dispatch(updateStockList(temporalStockList.stockList));
+
+      dispatch(updateTemporalStockListEditMode(false));
+      dispatch(resetTemporalStockList());
+    });
   };
 
-  return <EditButton isLock={disable} onClick={toggleLock} />;
+  return (
+    <EditButton disabled={disabled} isLock={disabled} onClick={toggleLock} />
+  );
 }
 
 export default StockListEditButton;
