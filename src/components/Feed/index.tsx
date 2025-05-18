6a42ef7a8d215cb73ed39d093da9654aa69f6bca
Song Hyun Kim <globalkmaria@gmail.com>
2025-05-18 15:24:43 +0900

    feat(home/news): add loading skeleton

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -11,9 +11,9 @@ import {
   FeedItemTitle,
   FeedList,
   FeedListItem,
-  FeedLoading,
   FeedTitle,
 } from './style';
+import { Skeleton2 } from '../Skeleton';
 
 interface Props {
   title: string;
@@ -22,6 +22,21 @@ interface Props {
 }
 
 function Feed({ title, showDescription, getFeed }: Props) {
+  return (
+    <FeedContainer>
+      <FeedTitle>{title}</FeedTitle>
+      <FeedBody
+        getFeed={getFeed}
+        title={title}
+        showDescription={showDescription}
+      />
+    </FeedContainer>
+  );
+}
+
+export default Feed;
+
+function FeedBody({ title, showDescription, getFeed }: Props) {
   const [isLoading, setIsLoading] = useState(true);
   const [result, setResult] = useState<ResultWithData<FeedItem[]>>();
 
@@ -37,30 +52,54 @@ function Feed({ title, showDescription, getFeed }: Props) {
     fetchFeed();
   }, [getFeed]);
 
+  if (isLoading) return <Loading showDescription={showDescription} />;
+
+  if (noContent)
+    return (
+      <FeedErrorMessage>{`${title} feed could not be loaded.`}</FeedErrorMessage>
+    );
+
   return (
-    <FeedContainer>
-      <FeedTitle>{title}</FeedTitle>
-      {isLoading ? (
-        <FeedLoading>Loading...</FeedLoading>
-      ) : noContent ? (
-        <FeedErrorMessage>{`${title} feed could not be loaded.`}</FeedErrorMessage>
-      ) : (
-        <FeedList>
-          {result?.data?.map((item, idx) => (
-            <FeedListItem key={idx}>
-              <a href={item.link} target='_blank' rel='noopener noreferrer'>
-                <FeedItemTitle>{item.title}</FeedItemTitle>
-                <FeedDate>{new Date(item.pubDate).toLocaleString()}</FeedDate>
-                {showDescription && (
-                  <FeedDescription>{item.description}</FeedDescription>
-                )}
-              </a>
-            </FeedListItem>
-          ))}
-        </FeedList>
-      )}
-    </FeedContainer>
+    <FeedList>
+      {result?.data?.map((item, idx) => (
+        <FeedListItem key={idx}>
+          <a href={item.link} target='_blank' rel='noopener noreferrer'>
+            <FeedItemTitle>{item.title}</FeedItemTitle>
+            <FeedDate>{new Date(item.pubDate).toLocaleString()}</FeedDate>
+            {showDescription && (
+              <FeedDescription>{item.description}</FeedDescription>
+            )}
+          </a>
+        </FeedListItem>
+      ))}
+    </FeedList>
   );
 }
 
-export default Feed;
+const array = Array.from({ length: 5 }, (_, i) => i + 1);
+
+function Loading({ showDescription }: Pick<Props, 'showDescription'>) {
+  return (
+    <FeedList>
+      {array.map(item => (
+        <FeedListItem key={item}>
+          <Skeleton2
+            width='300px'
+            height='23px'
+            margin='0 0 10px 0'
+            borderRadius='s'
+          />
+          <Skeleton2
+            width='150px'
+            height='17px'
+            margin='0 0 10px 0'
+            borderRadius='s'
+          />
+          {showDescription && (
+            <Skeleton2 width='100%' height='19px' borderRadius='s' />
+          )}
+        </FeedListItem>
+      ))}
+    </FeedList>
+  );
+}
